d72ead65e1dff603a6ddd9838e24d0e946c2fa77
Matrix <xhyrzldf@gmail.com>
2026-03-21 06:54:13 +0800

    fix: ModConfig v0.1.8 refresh mod image
    Update mod cover image for game mod browser.

--- MainFile.cs
+++ MainFile.cs
@@ -7,7 +7,7 @@ namespace ModConfig;
 public class MainFile
 {
     internal const string ModId = "sts2.piyixiajiuhenfen.modconfig";
-    internal const string Version = "0.1.7";
+    internal const string Version = "0.1.8";
     internal static readonly Logger Log = new(ModId, LogType.Generic);
 
     public static void Initialize()
